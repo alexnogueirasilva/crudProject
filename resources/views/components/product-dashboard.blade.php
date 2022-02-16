a1906141c0b50ce6ddd4e4e55f5a4a8a14b801c5
Alex Nogueira <alex@devaction.com.br>
2022-02-16 20:19:09 -0300

    correcting view dashboard

--- resources/views/components/product-dashboard.blade.php
+++ resources/views/components/product-dashboard.blade.php
@@ -7,20 +7,20 @@
                     <thead class="bg-gray-50">
                     <tr>
                         <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Product name') }}
                         </th>
                         <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Tags') }}
                         </th>
                         <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Status
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date create') }}
                         </th>
                         <th scope="col"
-                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date update') }}
                         </th>
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
+                        <th scope="col"
+                            class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <span>{{ __('Actions') }}</span>
                         </th>
                     </tr>
                     </thead>
@@ -40,19 +40,29 @@
                                 </div>
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap">
-                                <div class="text-sm text-gray-900">{{ $product->description }}</div>
-                            </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
                                 @foreach($product->tags as $tag)
                                     <span
                                         class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"><a
                                             href="{{ route('tag.edit', ['tag' => $tag->id]) }}">{{ $tag->name }}</a></span>
                                 @endforeach
                             </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div
+                                    class="text-sm text-gray-900">{{ date('d/m/Y H:i', strtotime($product->created_at))}}</div>
+                            </td>
 
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ \Illuminate\Support\Carbon::parse($product->created_at)->diffForHumans() }}</td>
+                            <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                <a href="{{ route('product.edit', ['product' => $product->id]) }}"
+                                   class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
+                                <form
+                                    action="{{ route('product.destroy', ['product' => $product->id]) }}"
+                                    method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit"
+                                            class="text-red-600 hover:text-red-900">{{ __('Delete') }}</button>
+                                </form>
                             </td>
 
                         </tr>
--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -19,79 +19,77 @@
             d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg></span>
                     </div>
                 @endif
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <!-- This example requires Tailwind CSS v2.0+ -->
-                <div class="flex flex-col">
-                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
-                                    <tr>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Name product') }}</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Description') }}</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date create') }}</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date update') }}</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            <span>{{ __('Actions') }}</span>
-                                        </th>
-                                    </tr>
-                                    </thead>
-                                    <tbody class="bg-white divide-y divide-gray-200">
-                                    @foreach($products as $product)
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <!-- This example requires Tailwind CSS v2.0+ -->
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead class="bg-gray-50">
                                         <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="flex items-center">
-                                                    <div class="flex-shrink-0 h-10 w-10">
-                                                        <img class="h-10 w-10 rounded-full"
-                                                             src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-                                                             alt="">
-                                                    </div>
-                                                    <div class="ml-4">
-                                                        <div
-                                                            class="text-sm font-medium text-gray-900">{{ $product->name }}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm text-gray-900">
-                                                    {{ $product->description }}
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <span
-                                                    class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"> {{ date('d/m/Y H:i', strtotime($product->created_at))}} </span>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ \Illuminate\Support\Carbon::parse($product->created_at)->diffForHumans() }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                                                <a href="{{ route('product.edit', ['product' => $product->id]) }}"
-                                                   class="px-6 py-4 text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
-                                                <form
-                                                    action="{{ route('product.destroy', ['product' => $product->id]) }}"
-                                                    method="POST">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit"
-                                                            class="px-6 py-4 text-red-600 hover:text-red-900">{{ __('Delete') }}</button>
-                                                </form>
-                                            </td>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Name product') }}</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date create') }}</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Date update') }}</th>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <span>{{ __('Actions') }}</span>
+                                            </th>
                                         </tr>
-                                    @endforeach
-                                    <!-- More people... -->
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach($products as $product)
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="flex items-center">
+                                                        <div class="flex-shrink-0 h-10 w-10">
+                                                            <img class="h-10 w-10 rounded-full"
+                                                                 src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                                                                 alt="">
+                                                        </div>
+                                                        <div class="ml-4">
+                                                            <div
+                                                                class="text-sm font-medium text-gray-900">{{ $product->name }}</div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="text-sm text-gray-900"></div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                <span
+                                                    class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ date('d/m/Y H:i', strtotime($product->created_at))}}</span>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ \Illuminate\Support\Carbon::parse($product->created_at)->diffForHumans() }}</td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                                    <a href="{{ route('product.edit', ['product' => $product->id]) }}"
+                                                       class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
+                                                    <form
+                                                        action="{{ route('product.destroy', ['product' => $product->id]) }}"
+                                                        method="POST">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button type="submit"
+                                                                class="text-red-600 hover:text-red-900">{{ __('Delete') }}</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        <!-- More people... -->
+                                        </tbody>
+                                    </table>
+                                </div>
+                                {{ $products->links() }}
                             </div>
-                            {{ $products->links() }}
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
 </x-app-layout>
