4ec74681585ad5c95db2eab287213a0d2704bf94
Alex Nogueira <alex@devaction.com.br>
2022-02-17 09:11:48 -0300

    Update Readme.md

--- app/Models/ProductsTags.php
+++ app/Models/ProductsTags.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property mixed $product_id
  * @property mixed $tag_id
+ * @method static create(array $all)
  */
 class ProductsTags extends Model
 {
